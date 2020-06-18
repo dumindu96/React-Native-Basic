499ffca6db2177c7d6f4c77c3a27e7d5ed772df5
dumindu96 <36619804+dumindu96@users.noreply.github.com>
2020-06-18 22:32:49 +0530

    start screen completed

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,20 +1,53 @@
 import React, {useState} from 'react';
-import {View, Button, StyleSheet, Text, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import {View, Button, StyleSheet, Text, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 
 import Card from '../components/Card';
 import Colors from '../constants/colors';
 import Input from '../components/Input';
+import NumberContainer from '../components/NumberContainer'
 
 const StartGameScreen = props => {
 
     const [enteredValue, setEnteredValue] = useState('');
+    const [confirmed, setConfirmed] = useState(false);
+    const [selectedNumber, setSelectedNumber] = useState();
 
     const numberInputHandler = inputText => {
         setEnteredValue(inputText.replace(/[^0-9]/g, ''));
     };
 
+    const resetInputHandler = () => {
+        setEnteredValue('');
+        setConfirmed(false);
+    };
+
+    const confirmInputHandler = () => {
+        const chosenNumber = parseInt(enteredValue);
+        if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99){
+            Alert.alert('Invalid Number!', 'Number has to be a number between 1 and 99.', [{text: "Okay", style:'destructive', onPress: resetInputHandler}])
+            return;
+        }
+        setConfirmed(true);
+        setSelectedNumber(chosenNumber);
+        setEnteredValue('');
+        Keyboard.dismiss();
+    };
+
+    let confirmedOutput;
+
+    if (confirmed) {
+        confirmedOutput = (
+        <Card style={styles.summaryContainer}>
+            <Text>You Selected</Text>
+            <NumberContainer>{selectedNumber}</NumberContainer>
+            {/* <View><Text>{selectedNumber}</Text></View> */}
+            <Button title="START GAME"/>
+        </Card>
+        );
+    }
+
     return(
-    <TouchableWithoutFeedback onPress={()=>{
+    <TouchableWithoutFeedback onPress={()=>{ 
         Keyboard.dismiss();
     }}>
         <View style={styles.screen}>
@@ -33,10 +66,11 @@ const StartGameScreen = props => {
                     value={enteredValue}
                 />
                 <View style={styles.buttonContainer}>
-                    <View style={styles.button}><Button title="Reset" onPress={() => {}} color="#ff0000" /></View>
-                    <View style={styles.button}><Button title="Confirm" onPress={() => {}} color={Colors.primary} /></View>
+                    <View style={styles.button}><Button title="Reset" onPress={resetInputHandler} color="#ff0000" /></View>
+                    <View style={styles.button}><Button title="Confirm" onPress={confirmInputHandler} color={Colors.primary} /></View>
                 </View>
             </Card>
+            {confirmedOutput}
         </View> 
       </TouchableWithoutFeedback> 
     );
@@ -70,6 +104,10 @@ const styles = StyleSheet.create({
     input:{
         width: 50,
         textAlign:'center',
+    },
+    summaryContainer: {
+        marginTop: 20,
+        alignItems: "center",
     }
 });
 
